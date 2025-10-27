68d81eb4bc4661b31d635bfd1c0e9be39417ab22
mdudek2 <mdudek2@hawk.iit.edu>
2025-10-27 16:58:45 -0500

    used css instead of semantic html to make text bold and italic

--- mdudek2-lab4.js
+++ mdudek2-lab4.js
@@ -7,6 +7,14 @@
   hero_h1.textContent = "Supercharge Your Brand with Stellar Marketing"
 
   // select the p in the hero and modify it's text
-  const hero_p = document.querySelector('#hero p')
-  hero_p.innerHTML = 'Leverage innovative strategies from Stellar Marketing to make your Business <strong><em>shine</em></strong> and <strong><em>succeed</em></strong>.';
+  const hero_p = document.querySelector('#hero p');
+  hero_p.innerHTML = 'Leverage innovative strategies from Stellar Marketing to make your Business <span class="italic_bold">shine</span> and <span class="italic_bold">succeed</span>.';
+
+  // Apply CSS via JavaScript
+  const italic_bold = document.querySelectorAll('.italic_bold');
+  italic_bold.forEach(span => {
+    span.style.fontWeight = 'bold';
+    span.style.fontStyle = 'italic';
+  });
+
 })();
