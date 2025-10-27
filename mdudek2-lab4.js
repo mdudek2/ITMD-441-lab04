9aaac76995022b51da210f58f993e0dbf42d3e3a
mdudek2 <mdudek2@hawk.iit.edu>
2025-10-27 16:51:10 -0500

    implemented first two page modifications

--- mdudek2-lab4.js
+++ mdudek2-lab4.js
@@ -1,1 +1,12 @@
+// IIFE function
 
+(function() {
+  
+  // select the h1 in the hero and modify it's text
+  const hero_h1 = document.querySelector('#hero h1');
+  hero_h1.textContent = "Supercharge Your Brand with Stellar Marketing"
+
+  // select the p in the hero and modify it's text
+  const hero_p = document.querySelector('#hero p')
+  hero_p.innerHTML = 'Leverage innovative strategies from Stellar Marketing to make your Business <strong><em>shine</em></strong> and <strong><em>succeed</em></strong>.';
+})();
